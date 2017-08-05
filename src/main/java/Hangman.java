8cfd6aad9da67957e40cadce53516b4465b902e7
charliekaks <kaksunchained@gmail.com>
2017-08-05 13:57:01 +0300

    complleted the second test to replace the characters withdashes

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,12 +1,39 @@
+import java.util.List;
 import java.util.Random;
+import java.util.Arrays;
 
 public class Hangman{
-    String [] wordsToGuess ={"cow", "goat", "leopard","lion","otter","horse"};
-
+  //the array that holds all the words in the game  
+List<String> wordsToGuess = Arrays.asList("cow", "goat", "leopard","lion","otter","horse");
+// intializing the random Object
     Random myRand = new Random();
-    String wordPicked = wordsToGuess[myRand.nextInt(wordsToGuess.length)];
-    public String findWord(){
+    //Randomly picking a word from the array
+    String wordPicked = wordsToGuess.get(myRand.nextInt(wordsToGuess.size()));
+    //the method that ensures tha a word is actually being gotten from the array.
+    public String findWord(String select){
         return wordPicked;
     }
+    //the method that replaces the words with blank dashes
+   public String replaceWordWithDashes(String dashes) {
+     String f = "";
+       //Spliting the word into an array of strings
+     String dashedWord[] =dashes.split("");
+       for (int das = 0; das < dashedWord.length; das++) {
+            dashedWord[das]="-";        
+       }
+        for(int fin=0; fin<dashedWord.length; fin++){
+          //converting the array into a string
+           f= f+ dashedWord[fin];
+        }
+          return f;
+   }
+        public String replaceDashesWithCorrectGuess(String letter) {
+          if(){
+            
+          }
+        }
+
+   
+
 
 }
--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -1,16 +1,31 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.Random;
+import java.util.Arrays;
+import java.util.List;
 
 public class HangmanTest{
-    String [] wordsToGuess ={"cow", "goat", "leopard","lion","otter","horse"};
 
-    Random myRand = new Random();
-    String wordPicked = wordsToGuess[myRand.nextInt(wordsToGuess.length)];
+    List<String>  wordsToGuess= Arrays.asList("cow", "goat", "leopard","lion","otter","horse");
+
+     Random myRand = new Random();
+   
     @Test
     public void findWord_itExpectsAWord_word(){
         Hangman testHangman = new Hangman();
-        String expected =
-        assertEquals();
+        String expected= testHangman.findWord(wordsToGuess.get(myRand.nextInt(wordsToGuess.size())));
+        assertEquals(true, wordsToGuess.contains(expected));
     } 
+    @Test
+    public void replaceWordWithDashes_itExpectsDashes_dash(){
+        Hangman testHangman = new Hangman();
+        String expected2 = "-------"; 
+        assertEquals(expected2,testHangman.replaceWordWithDashes("moringa"));
+    }
+    @Test
+    public void replaceDashesWithCorrectGuess_letter(){
+        Hangman testHangman = new Hangman();
+        String expected3 = "m-----";
+        assertEquals(expected3, testHangman.replaceDashesWithCorrectGuess("moringa"));
+    }
 }
